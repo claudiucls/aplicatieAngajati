3fa2dce63dfd4c7ee61b803e3c495594b7e89097
Claudiu <Claudiu@Claudiu-Desktop>
2016-11-09 18:11:18 +0200

    Commit modification

--- src/main/java/com/cls/repository/AngajatRepository.java
+++ src/main/java/com/cls/repository/AngajatRepository.java
@@ -4,10 +4,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import com.cls.entities.Angajat;
 import java.lang.String;
 
+@Repository
 public interface AngajatRepository extends JpaRepository<Angajat, Long>{
 	
 	@Query("select a from Angajat a where a.nume like %?1% or a.prenume like %?1% order by a.dataTerminarii desc NULLS FIRST, a.dataAngajarii desc")
